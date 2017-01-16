2c58c2f7c4fa0317dd07a0d4e262bb81496f8a15
cfen <chris.fenn@guardian.co.uk>
2017-01-16 19:22:47 +0000

    Added scrolling thermometer svg

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,8 @@ import degreeHTML from './text/degreePanel.html';
 
 import dataBase from './data/slideData.json';
 
-
 import { isMobile, isAndroidApp, splitString } from './utils';
+
 
 
 
@@ -27,6 +27,9 @@ var transTimeUnit = transTime/transScaleLength;
 
 function init(){
 	console.log(isMobile());
+
+	console.log(scrollDraw);
+
 	initData();	
 }
 
@@ -107,6 +110,9 @@ function initData(){
 
 
 	addSlidesView(slidesTimedObj);
+
+
+	scrollDraw();
 
 
 	initView(allEntriesArr);
@@ -365,6 +371,51 @@ function addInfoView(slidesData){
 	var tgtEl = document.querySelector('.gv-info-panel-holder');
 
 	tgtEl.innerHTML = tplOp;
+}
+
+
+
+function scrollDraw() {
+    // Get a reference to the <path>
+		var path = document.querySelector('#temperatureLine');
+
+		// Get length of path... ~577px in this case
+		var pathLength = path.getTotalLength();
+
+		// Make very long dashes (the length of the path itself)
+		path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+		// Offset the dashes so the it appears hidden entirely
+		path.style.strokeDashoffset = pathLength;
+
+		// Jake Archibald says so
+		// https://jakearchibald.com/2013/animated-line-drawing-svg/
+		path.getBoundingClientRect();
+
+		// When the page scrolls...
+		window.addEventListener("scroll", function(e) {
+		 
+		  // What % down is it? 
+		  // https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/2387222#2387222
+		  // Had to try three or four differnet methods here. Kind of a cross-browser nightmare.
+		  var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+		    
+		  // Length to offset the dashes
+		  var drawLength = pathLength * scrollPercentage;
+		  
+		  // Draw in reverse
+		  path.style.strokeDashoffset = pathLength - drawLength;
+		    
+		  // When complete, remove the dash array, otherwise shape isn't quite sharp
+		 // Accounts for fuzzy math
+		  if (scrollPercentage >= 0.99) {
+		    path.style.strokeDasharray = "none";
+		    
+		  } else {
+		    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+		  }
+		  
+		});
 }
 
 
